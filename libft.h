4e5b235e0bd07af9fb763c6b85cb0c4cddbe6255
Oussama Tahiri Alaoui <otahiri-@e1r4p4.1337.ma>
2025-10-14 11:51:59 +0100

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: otahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 10:26:27 by otahiri-          #+#    #+#             */
-/*   Updated: 2025/10/14 11:46:16 by otahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/14 11:51:47 by otahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int	ft_strlen(const char *str);
 int	ft_isalpha(char c);
+int	ft_isdigit(char c);
 
 #endif
