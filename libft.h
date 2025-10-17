4a9008f88c3e5ca169b4eae49219d32bb12f074f
Oussama Tahiri Alaoui <otahiri-@e1r4p4.1337.ma>
2025-10-17 13:04:56 +0100

    updated libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: otahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 10:26:27 by otahiri-          #+#    #+#             */
-/*   Updated: 2025/10/16 14:34:01 by otahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/17 11:58:39 by otahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,7 @@ void		*ft_calloc(size_t nmemb, size_t size);
 char		*ft_strdup(const char *s);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
 
 #endif
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-int main(void)
-{
-	calloc(-1,0 );
-}
